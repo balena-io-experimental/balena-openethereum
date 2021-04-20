59a88dbb7377251a36a94bd72960fc741aa46d0c
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-04-20 09:42:39 +0100

    Update to latest openethereum version

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p /mnt/ssd
 
 mount /dev/sda1 /mnt/ssd
 
-./openethereum --base-path=/mnt/ssd/openethereum --db-path=/mnt/ssd/openethereum-db
+./openethereum --base-path=/mnt/ssd/openethereum --db-path=/mnt/ssd/openethereum-db --pruning=fast --jsonrpc-interface=all --jsonrpc-hosts=all --warp-barrier=5620000
